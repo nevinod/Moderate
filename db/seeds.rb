3aef7356a63d02cb1ceb6c9eae7d48dac3cd50c9
Neil Vinod <neilvinod14@gmail.com>
2018-02-06 15:58:40 -0800

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(title: 'Fear not, China is not banning cryptocurrency', body: "In 2008 following the financial crisis, a paper titled “Bitcoin: A Peer-to-Peer Electronic Cash System” was published, detailing the concepts of a payment system. Bitcoin was born. Bitcoin gained the attention of the world for its use of blockchain technology and as an alternative to fiat currencies and commodities. Dubbed the next best technology after the internet, blockchain offered solutions to issues we have failed to address, or ignored over the past few decades. I will not delve into the technical aspect of it but here are some articles and videos that I recommend:
+
+How Bitcoin Works Under the Hood
+
+A gentle introduction to blockchain technology
+
+Ever wonder how Bitcoin (and other cryptocurrencies) actually work?
+
+Fast forward to today, 5th February to be exact, authorities in China have just unveiled a new set of regulations to ban cryptocurrency. The Chinese government have already done so last year, but many have circumvented through foreign exchanges. It has now enlisted the almighty ‘Great Firewall of China’ to block access to foreign exchanges in a bid to stop its citizens from carrying out any cryptocurrency transactions.
+
+To know more about the Chinese government stance, let’s backtrack a couple years back to 2013 when Bitcoin was gaining popularity among the Chinese citizens and prices were soaring. Concerned with the price volatility and speculations, the People’s Bank of China and five other government ministries published an official notice on December 2013 titled “Notice on Preventing Financial Risk of Bitcoin” (Link is in Mandarin). Several points were highlighted:
+
+1. Due to various factors such as limited supply, anonymity and lack of a centralized issuer, Bitcoin is not a official currency but a virtual commodity that cannot be used in the open market.
+
+2. All banks and financial organizations are not allowed to offer Bitcoin-related financial services or engage in trading activity related to Bitcoin.
+
+3. All companies and websites that offer Bitcoin-related services are to register with the necessary government ministries.
+
+4. Due to the anonymity and cross-border features of Bitcoin, organizations providing Bitcoin-related services ought to implement preventive measures such as KYC to prevent money laundering. Any suspicious activity including fraud, gambling and money laundering should to be reported to the authorities.
+
+5.Organizations providing Bitcoin-related services ought to educate the public about Bitcoin and the technology behind it and not mislead the public with misinformation.
+
+In layman’s term, Bitcoin is categorized as a virtual commodity (e.g in-game credits,) that can be bought or sold in its original form and not to be exchanged with fiat currency. It cannot be defined as money- something that serves as a medium of exchange, a unit of accounting, and a store of value.
+
+Despite the notice being dated in 2013, it is still relevant with regards to the Chinese government stance on Bitcoin and as mentioned, there is no indication of the banning Bitcoin and cryptocurrency. Rather, regulation and education about Bitcoin and blockchain will play a role in the Chinese crypto-market.
+
+A similar notice was issued on Jan 2017, again emphasizing that Bitcoin is a virtual commodity and not a currency. In September 2017, the boom of initial coin offerings (ICOs) led to the publishing of a separate notice titled “Notice on Preventing Financial Risk of Issued Tokens”. Soon after, ICOs were banned and Chinese exchanges were investigated and eventually closed. (Hindsight is 20/20, they have made the right decision to ban ICOs and stop senseless gambling). Another blow was dealt to China’s cryptocurrency community in January 2018 when mining operations faced serious crackdowns, citing excessive electricity consumption." , cover_img_url: "https://blog.giantswarm.io/assets/2015/04/Wikimedia_Servers-small.jpg")
