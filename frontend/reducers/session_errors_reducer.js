e57e89a600d4363aa20dd0bc60aab14b3cc0a9e2
Neil Vinod <neilvinod14@gmail.com>
2018-02-07 23:30:01 -0800

    session errors corrections

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -3,14 +3,14 @@ import {
   RECEIVE_CURRENT_USER,
 } from '../actions/session_actions';
 
-export default (state = [], action) => {
-  Object.freeze(state);
-  switch (action.type) {
-    case RECEIVE_SESSION_ERRORS:
-      return action.errors;
-    case RECEIVE_CURRENT_USER:
-      return null;
-    default:
-      return state;
-  }
-};
+// export default (state = [], action) => {
+//   Object.freeze(state);
+//   switch (action.type) {
+//     case RECEIVE_SESSION_ERRORS:
+//       return action.errors;
+//     case RECEIVE_CURRENT_USER:
+//       return null;
+//     default:
+//       return state;
+//   }
+// };
