9ae073cfc36b87c97a424ccd5545eca2cda8ce0d
Neil Vinod <neilvinod14@gmail.com>
2018-07-12 09:59:50 -0700

    test

--- frontend/components/articles/article_index_item.jsx
+++ frontend/components/articles/article_index_item.jsx
@@ -3,20 +3,6 @@ import { Link } from 'react-router-dom';
 
 
 const ArticleIndexItem = ({article, deleteArticle, blurb}) => (
-  <li className = "article-preview">
-    <div className="title-body-wrapper">
-      <Link className = "article-preview-title" to={`/articles/${article.id}`}>{article.title}</Link>
-      <p className="article-preview-body">{`${blurb}...`}</p>
-    </div>
-    <div className="image-box">
-      <img className="index-item-image" src={`${article.cover_img_url}`} />
-    </div>
-    {/*<button onClick={() => deleteArticle(article.id)}>Delete</button>*/}
-
-  </li>
-);
-
-export default ArticleIndexItem;
   <li>
     <Link className = "article-preview" to={`/articles/${article.id}`}>{article.title}>
       <div className="title-body-wrapper">
@@ -30,3 +16,9 @@ export default ArticleIndexItem;
 
     </Link>
   </li>
+
+);
+
+export default ArticleIndexItem;
+
+// hello
