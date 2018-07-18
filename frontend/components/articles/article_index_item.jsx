443affc59ecb9759c62656fb132b0f80bfb040e2
Neil Vinod <neilvinod14@gmail.com>
2018-07-18 15:52:43 -0700

    remove link

--- frontend/components/articles/article_index_item.jsx
+++ frontend/components/articles/article_index_item.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 
 const ArticleIndexItem = ({article, deleteArticle, blurb}) => (
   <li>
-    <Link className = "article-preview" to={`/articles/${article.id}`}>{article.title}>
+    // <Link className = "article-preview" to={`/articles/${article.id}`}>{article.title}>*/
+
+    <div className = "article-preview" >
       <div className="title-body-wrapper">
         <div className = "article-preview-title">
           <p className="article-preview-body">{`${blurb}...`}</p>
@@ -14,7 +16,7 @@ const ArticleIndexItem = ({article, deleteArticle, blurb}) => (
         <img className="index-item-image" src={`${article.cover_img_url}`} />
       </div>
       {/*<button onClick={() => deleteArticle(article.id)}>Delete</button>*/}
-    </Link>
+    </div>
   </li>
 
 );
