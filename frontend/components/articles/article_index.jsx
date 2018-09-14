784d3149679083ddabcbef083d83cfb5c1340914
Neil Vinod <neilvinod14@gmail.com>
2018-09-13 23:33:43 -0700

    package json update

--- frontend/components/articles/article_index.jsx
+++ frontend/components/articles/article_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ArticleIndexItem from './article_index_item';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class ArticleIndex extends React.Component {
   constructor(props) {
